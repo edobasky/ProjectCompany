24ee7d81fd3d62f9acbd75a453b5ee6293c8bbfe
edobasky <edore7@gmail.com>
2025-01-17 08:27:12 +0100

    added search function

---
+++ Repository/Extensions/RepositoryEmployeeExtension.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Extensions
+{
+    public static class RepositoryEmployeeExtension
+    {
+        public static IQueryable<Employees> FilterEmployees(this IQueryable<Employees> employees,uint minAge,uint maxAge) => employees.Where(e => (e.Age >= minAge && e.Age <= maxAge)); 
+
+        public static IQueryable<Employees> Search(this IQueryable<Employees> employees, string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm)) return employees;
+
+            var lowerCaseTerm = searchTerm.Trim().ToLower();
+
+            return employees.Where(e => e.Name!.ToLower().Contains(lowerCaseTerm));
+        }
+    }
+}
